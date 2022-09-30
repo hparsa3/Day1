578148d59faf862e0e7dcc715b2665a3b2cab91d
hparsa <hparsaa1985@gmail.com>
2022-09-29 18:16:57 -0700

    Day1

---
+++ Day1-HelloWorld/Day1-HelloWorld/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+using System.Collections.Generic;
+
+using System.Linq;
+
+using System.Text;
+
+using System.Threading.Tasks;
+
+
+
+namespace Day01Helloworld
+
+{
+
+    namespace ConsoleApp1
+
+    {
+
+        internal class Program
+
+        {
+
+            static void Main(string[] args)
+
+            {
+
+                Console.Write("what is your name?");
+
+                string name = Console.ReadLine();
+
+                Console.WriteLine("Hello" + " " + name + "!");
+
+                Console.ReadKey();
+
+            }
+
+        }
+
+    }
+
+}
